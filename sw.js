a4bb8d64d148d2b5bb9618a04d24536f23d3e536
Pluzhnikov Arseny <123958494+Nls0n@users.noreply.github.com>
2025-07-16 12:08:56 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,22 +1,11 @@
-const CACHE_NAME = 'portfolio-cache-v1';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/styles.css',
-  '/images/*',
-  '/icons/*'
-];
-
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
+self.addEventListener('install', (event) => {
+	console.log('Установлен');
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
-  );
+self.addEventListener('activate', (event) => {
+	console.log('Активирован');
+});
+
+self.addEventListener('fetch', (event) => {
+	console.log('Происходит запрос на сервер');
 });
